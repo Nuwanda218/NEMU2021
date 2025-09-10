36b41031de8ad3d910e7ffa49a5d221b6ec330de
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-10 06:30:40 +0000

    >  compile NEMU 3024244427 nuwanda Linux 3cbd1cb25f67 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  06:30:40 up  4:21,  0 users,  load average: 2.17, 2.11, 2.09 e9e6baff77ff13c915a7fa3331be947647b7f9a1

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -71,7 +71,7 @@ static struct {
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
     { "si", "Step execute N instructions (usage: si [N], default N=1)", cmd_si },
-	{ "info", "Display program status (usage: info r | info w)", cmd_info },
+	{ "info r", "Display program status", cmd_info },
 	
 
 	/* TODO: Add more commands */
