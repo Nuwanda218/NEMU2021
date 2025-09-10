ff6ddf616882b1ccdadf9c0083159c7e98a43fa7
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-10 04:14:12 +0000

    >  compile NEMU 3024244427 nuwanda Linux 3cbd1cb25f67 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:14:12 up  2:04,  0 users,  load average: 0.08, 0.02, 0.01 45d8a54dddab59458168e7728cfb0310d688fb35

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -35,7 +35,15 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
 	return -1;
 }
-
+static int cmd_si(char *args) {
+    int step = 1;   // Default step is 1
+    if (args != NULL) {
+        step = atoi(args);   // Convert argument to integer
+        if (step <= 0) step = 1; // Prevent invalid input
+    }
+    cpu_exec(step);  // Execute the given number of instructions
+    return 0;
+}
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +54,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+        { "si", "Step execute N instructions (usage: si [N], default N=1)", cmd_si },
 	/* TODO: Add more commands */
 
 };
