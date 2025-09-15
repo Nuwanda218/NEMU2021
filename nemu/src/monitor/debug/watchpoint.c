bec62b7657ebfc409c20507eb5fba070a19cc0fd
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-15 01:54:45 +0000

    >  compile NEMU 3024244427 nuwanda Linux 3cbd1cb25f67 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:54:45 up  5:51,  0 users,  load average: 0.00, 0.01, 0.04 4de45df053f9aedf5f92792a95f9c4e846c7098

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -125,29 +125,28 @@ void info_watchpoints() {
 bool check_watchpoints() {
     WP *wp = head;
     bool success = true;
-    bool triggered = false;
 
+    printf("[DEBUG] Checking watchpoint %d: expr=%s, last_val=%u\n", wp->NO, wp->expr, wp->last_val);
     while (wp != NULL) {
-        int new_val = expr(wp->expr, &success);
+        uint32_t new_val = expr(wp->expr, &success);
         if (!success) {
-            printf("Fail to evaluate expression for watchpoint %d: %s\n",
-                   wp->NO, wp->expr);
+            printf("Failed to evaluate expression for watchpoint %d: %s\n", wp->NO, wp->expr);
             wp = wp->next;
             continue;
         }
 
-        if (new_val != (int)wp->last_val) {
-            printf("Hint watchpoint %d at address 0x%08x\n", wp->NO, cpu.eip);
-            printf("Expression: %s\nOld value: %d\nNew value: %d\n",
-       		wp->expr, (int)wp->last_val, (int)new_val);
+        if (new_val != wp->last_val) {
+            printf("Hint watchpoint %d at address 0x%08x\n", wp->NO, (uint32_t)cpu.eip);
+            printf("Expression: %s\nOld value: %u\nNew value: %u\n",
+                   wp->expr, wp->last_val, new_val);
 
-            wp->last_val = new_val;  
+            wp->last_val = new_val;
             nemu_state = STOP;
-            triggered = true;
+            return true; 
         }
         wp = wp->next;
     }
-    return triggered;
+    return false;
 }
 
 /* TODO: Implement the functionality of watchpoint */
