b96f7e2e3f8c2853c98e16eacb509155b5ad2260
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-22 05:55:25 +0000

    >  compile NEMU 3024244427 nuwanda Linux 3cbd1cb25f67 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:55:25 up 12:12,  0 users,  load average: 3.16, 6.73, 11.50 cc1b7956ef4b81f6c191361c9cb3b60cd5854b3

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -1,12 +1,21 @@
 #include "cpu/exec/helper.h"
-#include "ret.h"
-
-#define DATA_BYTE 2
-#include "ret-template.h"
-#undef DATA_BYTE
-
-#define DATA_BYTE 4
-#include "ret-template.h"
-#undef DATA_BYTE
-
-make_helper_v(ret);
+ 
+//不需区分操作数位数，只需分析指令，因此不需要template.h文件
+make_helper(ret){
+    cpu.eip = swaddr_read(cpu.esp, 4) - 1;  //使eip跳转到esp中存放的返回地址
+    cpu.esp += 4;  //栈回退4位
+ 
+    print_asm("ret");
+ 
+    return 1;
+}
+ 
+make_helper(ret_i){
+    uint16_t imm = instr_fetch(eip + 1, 2);  //还需要读取ret后面所跟的16位的参数
+    cpu.eip = swaddr_read(cpu.esp, 4) - 1-2;  
+    cpu.esp += 4 + imm; 
+ 
+    print_asm("ret $0x%x", imm);
+ 
+    return 3;
+}
--- nemu/src/cpu/exec/control/ret.h
+++ nemu/src/cpu/exec/control/ret.h
@@ -1,10 +1,7 @@
-#ifndef __INSTR_RET_H__
-#define __INSTR_RET_H__
-
-#include "cpu/exec/helper.h"
-
-make_helper(ret_w);
-make_helper(ret_l);
-make_helper(ret_v);
-
+#ifndef __RET_H__
+#define __RET_H__
+ 
+make_helper(ret);
+make_helper(ret_i);
+ 
 #endif
