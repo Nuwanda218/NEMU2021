a4b6c342cd5289bc20a7f8d667d422516054a798
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-22 03:29:27 +0000

    >  compile NEMU 3024244427 nuwanda Linux 3cbd1cb25f67 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:29:27 up  9:46,  0 users,  load average: 17.41, 17.80, 15.38 d417ca10d44110b81e258809525c42aea1dcd45

--- nemu/src/cpu/exec/control/je-template.h
+++ nemu/src/cpu/exec/control/je-template.h
@@ -3,12 +3,16 @@
 #define instr je
 
 static void do_execute() {
-    if (cpu.eflags.ZF) {           // 注意这里
-        cpu.eip += op_src->val;
+    if (cpu.eflags.ZF) {
+        int offset = op_src->val;
+        printf("ZF is set, current EIP: %x, offset: %x\n", cpu.eip, offset);
+        cpu.eip += offset;
+        printf("Jumping to address: %x\n", cpu.eip);
+    } else {
+        printf("ZF is not set, not jumping\n");
     }
     print_asm_template1();
 }
-
 make_instr_helper(si)
 
 #include "cpu/exec/template-end.h"
