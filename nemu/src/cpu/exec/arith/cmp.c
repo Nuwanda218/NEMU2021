56b04fdaaa591e84aa8a690209a7e75ed0d29149
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-22 05:37:30 +0000

    >  compile NEMU 3024244427 nuwanda Linux 3cbd1cb25f67 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:37:30 up 11:54,  0 users,  load average: 15.92, 14.36, 14.68 fa495c4e91c0ce0e72b7ae24da9750f25f900537

--- nemu/src/cpu/exec/arith/cmp.c
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -1,19 +1,19 @@
 #include "cpu/exec/helper.h"
-
+ 
 #define DATA_BYTE 1
 #include "cmp-template.h"
 #undef DATA_BYTE
-
+ 
 #define DATA_BYTE 2
 #include "cmp-template.h"
 #undef DATA_BYTE
-
+ 
 #define DATA_BYTE 4
 #include "cmp-template.h"
 #undef DATA_BYTE
-
-/* 自动根据操作数大小选择 16/32 位版本 */
-make_helper(cmp_r2rm_v) { return (ops_decoded.is_operand_size_16 ? cmp_r2rm_w(eip) : cmp_r2rm_l(eip)); }
-make_helper(cmp_rm2r_v) { return (ops_decoded.is_operand_size_16 ? cmp_rm2r_w(eip) : cmp_rm2r_l(eip)); }
-make_helper(cmp_i2rm_v) { return (ops_decoded.is_operand_size_16 ? cmp_i2rm_w(eip) : cmp_i2rm_l(eip)); }
-make_helper(cmp_i2a_v)  { return (ops_decoded.is_operand_size_16 ? cmp_i2a_w(eip)  : cmp_i2a_l(eip)); }
+ 
+make_helper_v(cmp_i2a)
+make_helper_v(cmp_i2rm)
+make_helper_v(cmp_si2rm)
+make_helper_v(cmp_r2rm)
+make_helper_v(cmp_rm2r)
