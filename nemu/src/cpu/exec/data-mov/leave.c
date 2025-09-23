5d7822a134d218c2921866b86dc6a74b3747b9a8
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-23 11:00:08 +0000

    >  compile NEMU 3024244427 nuwanda Linux 3cbd1cb25f67 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:00:08 up 10 min,  0 users,  load average: 26.30, 23.67, 12.65 4ae663038cf707ceebc31a8b9aab174c2c95b5cf

---
+++ nemu/src/cpu/exec/data-mov/leave.c
@@ -0,0 +1,11 @@
+//不涉及操作数类型，在.c文件中定义make_helper函数框架即可
+#include "cpu/exec/helper.h"
+ 
+make_helper(leave){
+    cpu.esp = cpu.ebp;  //栈顶指向栈基
+    cpu.ebp = swaddr_read(cpu.esp, 4);  //栈基回到存放好的上一级函数的栈基
+    cpu.esp += 4;  //栈顶去除存放栈基的部分内存
+ 
+    print_asm("leave");
+    return 1;
+}
