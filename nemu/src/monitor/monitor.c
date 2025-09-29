2164a42610aeebe9b9b2e77507aedbc2bd06481d
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-29 09:12:44 +0000

    >  compile NEMU 3024244427 nuwanda Linux 3cbd1cb25f67 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:12:44 up 49 min,  0 users,  load average: 31.54, 31.31, 29.54 66ca60604f4166ae2f0df13cbefa31ca44225cf

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -86,7 +86,8 @@ void restart() {
 
 	/* Set the initial instruction pointer. */
 	cpu.eip = ENTRY_START;
-	cpu.eflags.val = 0x2;
+        cpu.eflags.val = 0x2;
+
 	/* Initialize DRAM. */
 	init_ddr3();
 }
